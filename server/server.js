497a3cd52f94829bf508fb70565c0796fb374c8c
diegoislas <diegoislas823@gmail.com>
2022-09-11 21:11:09 -0700

    added gitignore dependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "client/build")))
 
 app.get("/app", (req,res) => {
     res.json({"user": "johnny"})
